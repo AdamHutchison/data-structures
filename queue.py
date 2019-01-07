8953d70fbe189e01bcc29a2e00577004ee2a536d
adam.hutchison <adam.hutchison@netsells.co.uk>
2019-01-07 13:04:03 +0000

    Adds Dequeue data structure

--- queue.py
+++ queue.py
@@ -2,4 +2,19 @@ class Queue:
     def __init__(self):
         self.items = []
 
-    def 
+    def queue(self, item):
+        self.items.insert(0, item)
+    
+    def dequeue(self):
+        if self.items:
+            return self.items.pop()
+
+    def peek(self):
+        if self.items:
+            return self.items[-1]
+    
+    def size(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return len(self.items) == 0
